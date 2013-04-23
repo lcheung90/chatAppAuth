e1f3a6544a31b4df9cc469bbd7013a39455f3d9b
Luis Cheung <lcheung90@gmail.com>
2013-04-22 18:21:41 -0700

    Populate sessions controller actions
    using code from bookmarks app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	@user = User.find_by_username(params[:session][:username].downcase)
+		if @user && @user.authenticate(params[:session][:password])
+			reset_session
+			session[:user_id] = @user.id
+			flash[:notice] = "Successfully Logged in"
+			redirect_to user_url(params[:session][:username])
+		else
+			flash.now[:notice] = "Please provide valid credentials"
+			render :new
+		end
   end
 
   def destroy
+    reset_session
+    flash[:notice] = "Successfully Logged out"
+    redirect_to root_url
   end
 end
