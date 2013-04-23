d07cad90c2dea4c4ab803aa3ae0d0df46eccc150
Luis Cheung <lcheung90@gmail.com>
2013-04-22 18:26:19 -0700

    Populate users controller actions
    using bookmarks app code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+        reset_session
+        session[:user_id] = @user.id
+        flash[:notice] = "User successfully created"
+        redirect_to user_url(@user.username)
+    else
+        render action: "new"
+    end
   end
 
   def show
+    @user = User.find_by_username(params[:id])
   end
 end
