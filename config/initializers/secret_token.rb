86b579d46ec47eb2c4898e9a8b27be5370fd5a82
Luis Cheung <lcheung90@gmail.com>
2013-04-22 18:10:51 -0700

    Initial commit for chatAppAuth
    create sessions and users controllers
    add gems to gem file
    add user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatAppAuth::Application.config.secret_token = 'fe3bcd6db4a001ef1f217d27ef5d8620959df27eb0cad82d90e5eb2ba19513ffc659ac268122a5770976b5fbc63e58a58cdd9626f57ae783cffb1a8844e6395e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChatAppAuth::Application.config.session_store :cookie_store, key: '_chatAppAuth_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChatAppAuth::Application.config.session_store :active_record_store
