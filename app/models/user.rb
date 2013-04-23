bd5aeeb076f1530dcee3a6f1885cf4b1e2086263
Luis Cheung <lcheung90@gmail.com>
2013-04-22 18:14:55 -0700

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :username
+  attr_accessible :password,:password_confirmation, :username
+  has_secure_password
+  validates_confirmation_of :password
+  validates_presence_of :password, :on =>:create
+  validates_presence_of :username
+  validates_uniqueness_of :username
 end
